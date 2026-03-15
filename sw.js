258172fb6cd8db5d3e25f76983e710bff079f2b9
vataliyavatsal-ctrl <vataliyavatsal@gmail.com>
2026-03-15 15:51:24 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'vira-v1';
+const urlsToCache = [
+  '/virasafe/',
+  '/virasafe/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
